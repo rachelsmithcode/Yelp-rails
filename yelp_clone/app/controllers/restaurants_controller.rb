91899eacae65803ac600184c79176c9fdb2dff03
rachelsmithcode <rachelcodesmith@outlook.com>
2016-02-29 17:25:45 +0000

    second test to create new restaurant passing

--- yelp_clone/app/controllers/restaurants_controller.rb
+++ yelp_clone/app/controllers/restaurants_controller.rb
@@ -1,7 +1,20 @@
 class RestaurantsController < ApplicationController
 
   def index
-    # raise 'Hello from index action.'
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def restaurant_params
+     params.require(:restaurant).permit(:name)
   end
 
 end
--- yelp_clone/spec/features/restaurant_feature_spec.rb
+++ yelp_clone/spec/features/restaurant_feature_spec.rb
@@ -5,9 +5,19 @@ feature 'Restaurants' do
     scenario 'should display a prompt to add a restaurant' do
         visit '/restaurants'
         expect(page).to have_content('No restaurants have been added')
-        expect(page).to have_link('add a restaurant')
+        expect(page).to have_link('Add a restaurant')
     end
+  end
 
+  context 'creating restaurants' do
+    scenario 'prompt users to fill out a form to create a new restaurant' do
+        visit '/restaurants'
+        click_link 'Add a restaurant'
+        fill_in 'Name', with: 'Cafe Rouge'
+        click_button 'Create Restaurant'
+        expect(page).to have_content('Cafe Rouge')
+        expect(current_path).to eq('/restaurants')
+    end
   end
 
 end
