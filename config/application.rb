9a99f53af3aa76378c39e0c6c6a96309650f3b00
rachelsmithcode <rachelcodesmith@outlook.com>
2016-03-03 11:44:08 +0000

    can sign in with facebook

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,13 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Dotenv::Railtie.load
+
+HOSTNAME = ENV['HOSTNAME']
+
 module YelpClone
   class Application < Rails::Application
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
